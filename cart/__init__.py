0e8bf5a7ed732513bbd14a3808b3c5be86f0814b
大哆哆 <49628702+daduoduo123@users.noreply.github.com>
2021-03-22 10:55:23 +0800

    Update __init__.py

---
+++ cart/__init__.py
@@ -0,0 +1,12 @@
+来电应答方式：接听、拒接、短信拒接、忽略来电超时、无通话接听（异常情况）
+主叫、被叫
+主卡、副卡
+音频、视频（通话升降级请求）
+UI、Interface、robot（调用方式）
+单路通话、两路通话
+呼叫联系人、呼叫历史记录、回拨未接来电
+飞行模式（异常呼叫）
+ims的注册域：LTE NR WIFI GSM UTRAN NOIMS
+通话状态：IDLE DIALING ALERTING INCOMING ACTIVE WAITING HOLDING CONFERENCE
+短信类型：已发送、收件箱、草稿箱
+呼叫转移类型：无条件呼叫转移、遇忙转呼
