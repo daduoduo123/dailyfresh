61d146a301e8e243c0e9ed31829715dea1243b7a
大哆哆 <49628702+daduoduo123@users.noreply.github.com>
2021-03-22 11:38:46 +0800

    Update __init__.py

--- cart/__init__.py
+++ cart/__init__.py
@@ -1,12 +1,19 @@
 来电应答方式：接听、拒接、短信拒接、忽略来电超时、无通话接听（异常情况）
-主叫、被叫
+主叫、被叫、电话会议
+单卡、双卡
 主卡、副卡
+运营商：电信、移动、网通
 音频、视频（通话升降级请求）
 UI、Interface、robot（调用方式）
-单路通话、两路通话
+单路通话、两路通话、目前尝试过四路通话、进行通话合并（进入电话会议）、可拆除
 呼叫联系人、呼叫历史记录、回拨未接来电
 飞行模式（异常呼叫）
-ims的注册域：LTE NR WIFI GSM UTRAN NOIMS
+ims的注册域：LTE NR WIFI GSM UTRAN NOIMS（同ims域通话，不同ims域通话，ims域和非ims域通话）
 通话状态：IDLE DIALING ALERTING INCOMING ACTIVE WAITING HOLDING CONFERENCE
 短信类型：已发送、收件箱、草稿箱
 呼叫转移类型：无条件呼叫转移、遇忙转呼
+触发ECT业务（不清楚做啥）
+发送DTMF音
+切换通话
+
+
